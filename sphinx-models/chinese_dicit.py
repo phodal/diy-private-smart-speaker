5506cef892163274076d44c220926a5af7733ab8
Phodal Huang <h@phodal.com>
2017-08-28 11:25:41 +0800

    [T] thinking in chinese dicit

---
+++ sphinx-models/chinese_dicit.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+# author: binux(17175297.hk@gmail.com)
+# https://github.com/binux/binux-tools/blob/master/python/chinese_digit.py
+
+dict ={u'零':0, u'一':1, u'二':2, u'三':3, u'四':4, u'五':5, u'六':6, u'七':7, u'八':8, u'九':9, u'十':10, u'百':100, u'千':1000, u'万':10000,
+       u'０':0, u'１':1, u'２':2, u'３':3, u'４':4, u'５':5, u'６':6, u'７':7, u'８':8, u'９':9,
+                u'壹':1, u'贰':2, u'叁':3, u'肆':4, u'伍':5, u'陆':6, u'柒':7, u'捌':8, u'玖':9, u'拾':10, u'佰':100, u'仟':1000, u'萬':10000,
+       u'亿':100000000}
+
+def getResultForDigit(a, encoding="utf-8"):
+    if isinstance(a, str):
+        a = a.decode(encoding)
+
+    count = 0
+    result = 0
+    tmp = 0
+    Billion = 0
+    while count < len(a):
+        tmpChr = a[count]
+        #print tmpChr
+        tmpNum = dict.get(tmpChr, None)
+        #如果等于1亿
+        if tmpNum == 100000000:
+            result = result + tmp
+            result = result * tmpNum
+            #获得亿以上的数量，将其保存在中间变量Billion中并清空result
+            Billion = Billion * 100000000 + result
+            result = 0
+            tmp = 0
+        #如果等于1万
+        elif tmpNum == 10000:
+            result = result + tmp
+            result = result * tmpNum
+            tmp = 0
+        #如果等于十或者百，千
+        elif tmpNum >= 10:
+            if tmp == 0:
+                tmp = 1
+            result = result + tmpNum * tmp
+            tmp = 0
+        #如果是个位数
+        elif tmpNum is not None:
+            tmp = tmp * 10 + tmpNum
+        count += 1
+    result = result + tmp
+    result = result + Billion
+    return result
--- sphinx-models/genearate_keywords.py
+++ sphinx-models/genearate_keywords.py
@@ -3,8 +3,8 @@ import jieba
 output = open('output.txt', 'w')
 with open("basic.txt") as f:
     for line in f:
-    	seg_list = jieba.cut(line)
-    	seg_list_with_split = " ".join(seg_list)
-    	output.write(seg_list_with_split)
+        seg_list = jieba.cut(line)
+        seg_list_with_split = " ".join(seg_list)
+        output.write(seg_list_with_split)
 
 output.close()
